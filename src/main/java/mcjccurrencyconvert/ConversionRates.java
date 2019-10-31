5c573922e36cbdeabcedf08dc4ccbd5b7e9791c3
cameron.cagle <cameron.cagle@TRNCLT31348.fdmgroup.local>
2019-10-31 14:36:49 -0400

    Finished ConversionRates

--- src/main/java/mcjccurrencyconvert/ConversionRates.java
+++ src/main/java/mcjccurrencyconvert/ConversionRates.java
@@ -1,50 +1,93 @@
 package mcjccurrencyconvert;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
 import org.jdom2.input.DOMBuilder;
+import org.jdom2.input.SAXBuilder;
 import org.xml.sax.SAXException;
 
 public class ConversionRates {
 	private Map<String, BigDecimal> conversionRates;
 	private Document xmlConversion;
-	
-	public ConversionRates( ) {
-		xmlConversion = makeConversionRates();
-	}
-	
-	private Document makeConversionRates() {
+
+	public ConversionRates() {
 		xmlConversion = null;
+		conversionRates = new HashMap<String, BigDecimal>();
+	}
+
+	public Optional<InputStream> makeXMLDocument(String urlLocation) {
+		InputStream input = null;
+		if (urlLocation.equals("")) {
+			return Optional.ofNullable(input);
+		}
+		URL xmlURL;
 		try {
-			URL xmlURL = new URL("http://wwww.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+			xmlURL = new URL(urlLocation);
 			InputStream xml = xmlURL.openStream();
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			org.w3c.dom.Document w3cDocument = db.parse(xml);
-			xmlConversion = new DOMBuilder().build(w3cDocument);
-		} catch (IOException | SAXException | ParserConfigurationException e) {
+			input = xml;
+		} catch (IOException e) {
+			File file = new File(urlLocation);
+			if (file.exists()) {
+				try {
+					InputStream newFile = new FileInputStream(file);
+					input = newFile;
+				} catch (FileNotFoundException fne) {
+					fne.printStackTrace();
+					throw new IllegalArgumentException("Invalid argument");
+				}
+			} else {
+				throw new IllegalArgumentException("Invalid argument");
+			}
+		}
+		return Optional.ofNullable(input);
+	}
+
+	public Optional<Document> parseXMLDocument(InputStream in) {
+		SAXBuilder builder = new SAXBuilder();
+		try {
+			xmlConversion = builder.build(in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JDOMException e) {
 			e.printStackTrace();
 		}
-		return xmlConversion;
+		return Optional.ofNullable(xmlConversion);
 	}
-	
-	private void readConversionRates() {
+
+	public void readConversionRates() {
 		Element rootNode = xmlConversion.getRootElement();
-		System.out.println("Root Element :: " + rootNode.getName());
-		System.out.println("Root Child :: " + rootNode.getAttributeValue("cube rate"));
+		List<Element> children = rootNode.getChild("Cube", null).getChild("Cube", null).getChildren();
+		for (Element element : children) {
+			BigDecimal value = new BigDecimal(element.getAttributeValue("rate"));
+			conversionRates.put(element.getAttributeValue("currency"), value);
+		}
 	}
-	
+
 	public BigDecimal getConversionRate(String currencyName) {
 		return conversionRates.get(currencyName);
+	}
+	
+	public void setConversionRates(Map<String, BigDecimal> map) {
+		this.conversionRates = map;
 	}
 }
