57bf63db025d47f29c14bfe0ccddeb862a995b50
Justin.Tooley <Justin.Tooley@TRNCLT31228.fdmgroup.local>
2019-10-31 09:47:36 -0400

    Added Override annotation to methods that implement conversions

--- src/main/java/mcjccurrencyconvert/ToEuroConversion.java
+++ src/main/java/mcjccurrencyconvert/ToEuroConversion.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class ToEuroConversion implements Conversion {
+	@Override
 	public BigDecimal convert(BigDecimal amount, BigDecimal conversionRate) {
 		return amount.divide(conversionRate,2,RoundingMode.HALF_UP);
 	}
