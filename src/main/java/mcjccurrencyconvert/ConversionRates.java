bbca922c4e9d91dccaafa9f509755a09d77e956c
cameron.cagle <cameron.cagle@TRNCLT31348.fdmgroup.local>
2019-10-31 09:45:04 -0400

    Added Conversion Rates First Draft

--- src/main/java/mcjccurrencyconvert/ConversionRates.java
+++ src/main/java/mcjccurrencyconvert/ConversionRates.java
@@ -1,7 +1,50 @@
 package mcjccurrencyconvert;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.DOMBuilder;
+import org.xml.sax.SAXException;
+
 public class ConversionRates {
-	private static Document conversionRates;
+	private Map<String, BigDecimal> conversionRates;
+	private Document xmlConversion;
 	
-	private static Document createDocument(
+	public ConversionRates( ) {
+		xmlConversion = makeConversionRates();
+	}
+	
+	private Document makeConversionRates() {
+		xmlConversion = null;
+		try {
+			URL xmlURL = new URL("http://wwww.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+			InputStream xml = xmlURL.openStream();
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			org.w3c.dom.Document w3cDocument = db.parse(xml);
+			xmlConversion = new DOMBuilder().build(w3cDocument);
+		} catch (IOException | SAXException | ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		return xmlConversion;
+	}
+	
+	private void readConversionRates() {
+		Element rootNode = xmlConversion.getRootElement();
+		System.out.println("Root Element :: " + rootNode.getName());
+		System.out.println("Root Child :: " + rootNode.getAttributeValue("cube rate"));
+	}
+	
+	public BigDecimal getConversionRate(String currencyName) {
+		return conversionRates.get(currencyName);
+	}
 }
